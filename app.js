ad585b5d2e4a12e3cbbb893725d491dea1eca631
peppyeben <65021503+peppyeben@users.noreply.github.com>
2022-05-21 16:57:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -15,19 +15,24 @@ const options = [
 
 let selectedTier = null
 let investmentMessage
-let investor
+let investor = null
 
 const tiers = document.querySelector("#tiers")
 const userName = document.querySelector("#username")
 const message = document.querySelector("#message")
+const errorMessage = document.querySelector("#error-message")
 const submit = document.querySelector("#submit")
+let newTier = null
 
 tiers.addEventListener("change", (e)=> {
 	selectedTier = e.target.value
 	c = options.filter(a => {
 		return a[selectedTier]
 	})
-	investmentMessage = `You will invest ${c[0][selectedTier]} for a ROI of ${c[0]["percent"]} over the period of a week%`
+	newTier = c
+	investmentMessage = `You will invest ${c[0][selectedTier]} for a ROI of ${c[0]["percent"]} % over the period of a week`
+	message.innerText = investmentMessage
+	errorMessage.innerText = ``
 })
 
 userName.addEventListener("input", (e) => {
@@ -35,16 +40,22 @@ userName.addEventListener("input", (e) => {
 })
 
 submit.addEventListener("click", () => {
-	if(selectedTier != null && investor != "") {
-		console.log("hello")
+	console.log(selectedTier)
+	console.log(investor)
+	if(selectedTier != null && investor != "" && investor != null) {
+		investmentMessage = `Thanks for investing ${investor},
+		You have successfully invested ${newTier[0][selectedTier]} for a period of one week. You will receive a return of ${newTier[0]["percent"]}%
+		`
+		message.innerText = investmentMessage
+		errorMessage.innerText = ``
+	} else {
+		errorMessage.innerText = `Please fill in all fields`
 	}
 })
 
 function init () {
 	tiers.selectedIndex = -1
-	selectedTier = tiers.value
+	selectedTier = null
 }
 
 init()
-
-// console.log(tiers.value)
